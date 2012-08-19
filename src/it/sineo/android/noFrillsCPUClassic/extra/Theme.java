19555ec88cafc76e94d52f3a987d4c02002ff66b
Luca Santarelli <luca.santarelli@gmail.com>
2012-08-19 22:58:41 +0200

    Fix: different handling of themes depending on presence of ActionBar

--- src/it/sineo/android/noFrillsCPUClassic/extra/Theme.java
+++ src/it/sineo/android/noFrillsCPUClassic/extra/Theme.java
@@ -2,19 +2,26 @@ package it.sineo.android.noFrillsCPUClassic.extra;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.preference.PreferenceManager;
 
 public enum Theme {
 	/*
-	 * Classic can't handle LIGHT+DARK (since there is no action bar!) but in
-	 * order to avoid breakages, we put a fake third element in the enumeration.
+	 * Light + Dark action bar har no meaning when on SDK<=10, since there is no
+	 * action bar at all.
 	 */
-	THEME_DARK(android.R.style.Theme_Black), THEME_LIGHT(android.R.style.Theme_Light), THEME_LIGHT_DARK(android.R.style.Theme_Light);
+	THEME_DARK(android.R.style.Theme_Black, android.R.style.Theme_Holo), THEME_LIGHT(android.R.style.Theme_Light,
+			android.R.style.Theme_Holo_Light), THEME_LIGHT_DARK(android.R.style.Theme_Light,
+			android.R.style.Theme_Holo_Light_DarkActionBar);
 	int themeId;
 	int descriptionId;
 
-	private Theme(int themeId) {
-		this.themeId = themeId;
+	private Theme(int themeId, int v11ThemeId) {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+			this.themeId = v11ThemeId;
+		} else {
+			this.themeId = themeId;
+		}
 	}
 
 	public static Theme applyTo(Context context) {
